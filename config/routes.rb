ac10879b0366cab85362a8d615119ba35c9f0ac4
christycui <christycui14@gmail.com>
2015-04-07 21:04:14 +0000

    app setup and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'ui#index'
+  get 'ui/(:action)', controller: 'ui'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
