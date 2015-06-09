15a5634e518b4a375cd4acb097466c2b888acc9e
christycui <christycui14@gmail.com>
2015-06-09 22:31:21 +0000

    user can change time zone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609202420) do
+ActiveRecord::Schema.define(version: 20150609213157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150609202420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.string   "time_zone"
   end
 
 end
